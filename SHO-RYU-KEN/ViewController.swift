8e8ca53cf3bd96e36a99f24436a9e9b9ab3a80b5
sayaka310 <syk3887@yahoo.co.jp>
2021-11-02 13:41:03 +0900

    Initial Commit

---
+++ SHO-RYU-KEN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SHO-RYU-KEN
+//
+//  Created by user1 on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
