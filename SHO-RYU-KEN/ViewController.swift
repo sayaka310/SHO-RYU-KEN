5e228794f87a4e8648f7fe392ee49886c3c81c0f
sayaka310 <syk3887@yahoo.co.jp>
2021-11-02 22:36:44 +0900

    課題完了

--- SHO-RYU-KEN/ViewController.swift
+++ SHO-RYU-KEN/ViewController.swift
@@ -14,6 +14,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var dispImageNo = 0
+    let images = ["attak1", "attak2", "attak3", "attak4", "attak5", "attak6", "attak7", "attak8", "attak9", "attak10", "attak11", "attak12", "attak13", "attak14", "attak15", "attak16", "attak17", "attak18", "attak19", "attak20"]
+    
+    @IBAction func actionButtun(_ sender: Any) {
 
+        imageView.animationImages = dispImages(for: images)
+        imageView.animationDuration = 0.9
+        imageView.animationRepeatCount = 1
+        imageView.image = imageView.animationImages?.first
+        imageView.startAnimating()
+    
+    }
+    
+    func dispImages(for name: [String]) -> [UIImage] {
+        
+        var images = [UIImage]()
+        for image_name in name {
+            if let image = UIImage(named: image_name) {
+                images.append(image)
+            }
+        }
+        return images
+    }
 }
 
